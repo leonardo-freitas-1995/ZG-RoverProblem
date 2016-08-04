11703855282dc13461ae2c70d1637da84b660afd
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-08-04 20:12:59 -0300

    Implementando o método moveRover para CircleTerrain

--- src/zg/entities/CircleTerrain.java
+++ src/zg/entities/CircleTerrain.java
@@ -5,6 +5,10 @@ package zg.entities;
  */
 public class CircleTerrain extends Terrain {
 
+	// Movement unit constants
+	private final static int ARC_MOVEMENT_UNIT = 1;
+	private final static int RADIUS_MOVEMENT_UNIT = 1;
+
 	private final int radius;
 
 	public CircleTerrain(int radius) {
@@ -13,6 +17,29 @@ public class CircleTerrain extends Terrain {
 
 	@Override
 	public Coordinate moveRover(Coordinate pos) {
-		return null;
+		int r = pos.getR();
+		double angle = pos.getAngle();
+		switch (pos.getOrientation()){
+			case Coordinate.NORTH:
+				r += RADIUS_MOVEMENT_UNIT;
+				break;
+			case Coordinate.EAST:
+				angle += (ARC_MOVEMENT_UNIT / this.radius);
+				break;
+			case Coordinate.SOUTH:
+				r -= RADIUS_MOVEMENT_UNIT;
+				break;
+			case Coordinate.WEST:
+				angle -= (ARC_MOVEMENT_UNIT / this.radius);
+				break;
+		}
+		if (angle > Math.PI * 2){
+			angle -= Math.PI * 2;
+		}
+		if (r < 0 || r > this.radius){
+			return null;
+		}
+
+		return new Coordinate(r, angle, pos.getOrientation());
 	}
 }
