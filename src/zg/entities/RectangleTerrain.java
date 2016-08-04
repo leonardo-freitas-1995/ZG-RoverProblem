2a02cd1666f02973000ccd8f3b2ead23bdb3ec47
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-08-04 18:01:25 -0300

    Refinando terreno retangular

--- src/zg/entities/RectangleTerrain.java
+++ src/zg/entities/RectangleTerrain.java
@@ -5,6 +5,14 @@ package zg.entities;
  */
 public class RectangleTerrain extends Terrain {
 
+	private final int maxHorizontal;
+	private final int maxVertical;
+
+	public RectangleTerrain(int maxHorizontal, int maxVertical) {
+		this.maxHorizontal = maxHorizontal;
+		this.maxVertical = maxVertical;
+	}
+
 	@Override
 	public Coordinate moveRover(Coordinate pos) {
 		int x = pos.getX();
@@ -22,6 +30,11 @@ public class RectangleTerrain extends Terrain {
 			case Coordinate.WEST:
 				x--;
 				break;
+		}
+
+		if (x < 0 || x > maxHorizontal ||
+			y < 0 || y > maxVertical){
+			return null;
 		}
 
 		return new Coordinate(x + " " + y + " " + pos.getOrientation(), Coordinate.RECTANGLE_COORDINATE);
